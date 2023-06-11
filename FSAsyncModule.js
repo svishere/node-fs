8a74e90fbfaf165805733e913dd0992b1509c560
svishere <sakthivel100gym@gmail.com>
2023-06-11 07:47:37 +0530

    first commit

---
+++ FSAsyncModule.js
@@ -0,0 +1,46 @@
+  exports.FSAsync=()=>  {const {readFile, writeFile} = require('fs')
+    console.log('Start');
+
+    readFile('./Files/first.txt', 'utf-8' , (error , data) => {
+    if(error)
+    {
+        console.log(error);
+    }
+    const firstFile = data
+    console.log('Variable Assigned 1');
+
+
+    writeFile('./Files/firstWrittenAndAsyncFile.txt' , `First written async file : ${firstFile}`, (error, data) => {
+        if(error)
+        {
+            console.log(error);
+        }
+        console.log('Task Completed 1');
+    })
+    })
+
+    console.log('Started new Task 1');
+    console.log('End 1');
+
+    readFile('././Files/first.txt', 'utf-8' , (error , data) => {
+    if(error)
+    {
+        console.log(error);
+    }
+    const firsFile = data
+    console.log('Variable Assigned 2');
+
+
+    writeFile('./Files/firstWritten&AsyncFile.txt' , `First written async file : ${firsFile}`, (error, data) => {
+        if(error)
+        {
+            console.log(error);
+        }
+        console.log('Task Completed 2');
+    })
+    })
+
+
+    console.log('Started new Task 2');
+    console.log('End 2');
+    }
---
+++ app.js
@@ -0,0 +1,2 @@
+const FS=require('./FSAsyncModule')
+FS.FSAsync()
